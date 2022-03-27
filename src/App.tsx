8224db1078276772e55a781b7fe4153e2fe793d6
Suraj Vijay <surajcpp115@gmail.com>
2022-03-28 05:15:07 +0530

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
   return (
-    <div>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/*" element={() => <h1>404</h1>} />
+      </Routes>
+    </HashRouter>
   );
-}
+};
 
 export default App;

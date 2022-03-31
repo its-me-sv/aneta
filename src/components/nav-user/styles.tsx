fba68c2f8ce6ff0b6686414d1c52af0d7b63aeb9
Suraj Vijay <surajcpp115@gmail.com>
2022-03-31 22:23:15 +0530

    fixed styling for user navigation bar

--- src/components/nav-user/styles.tsx
+++ src/components/nav-user/styles.tsx
@@ -3,17 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
   background-color: #1977f3;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export const Body = styled.div`
   display: flex;
   flex-direction: column;
-  position: absloute;
   padding: 7%;
 `;
 
 export const Footer = styled.div`
-  position: absolute;
-  bottom: 0;
-  width: 100%;
 `;

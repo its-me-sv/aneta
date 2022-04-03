d5d4d5177b4e8fea275ffe361de37f1548cf5fb2
Suraj Vijay <surajcpp115@gmail.com>
2022-04-03 12:19:55 +0530

    timeago format on joined on section

--- src/components/joined-on/index.tsx
+++ src/components/joined-on/index.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import {format} from 'timeago.js';
 
 import Section, {SectionCaption} from '../section';
 import prettyDate from "../../utils/pretty-date";
 import {getDateObj} from "../../utils/timeuuid-to-date";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const SinceFormatted = styled.span`
+  font-family: bahnschrift;
+  font-size: 1rem;
+  opacity: 0.7;
+`;
+
 interface JoinedOnProps {}
 
 const JoinedOn: React.FC<JoinedOnProps> = () => {
+    const timeStamp = getDateObj(
+      "9467eb20-a622-11ec-9631-773bd57f3429"
+    ).toISOString();
     return (
       <div>
         <Section name="Joined on" />
-        <SectionCaption>
-          {prettyDate(
-            getDateObj("9467eb20-a622-11ec-9631-773bd57f3429").toISOString()
-          )}
-        </SectionCaption>
+        <Container>
+          <SectionCaption>{prettyDate(timeStamp)}</SectionCaption>
+          <SinceFormatted>{format(timeStamp)}</SinceFormatted>
+        </Container>
       </div>
     );
 };

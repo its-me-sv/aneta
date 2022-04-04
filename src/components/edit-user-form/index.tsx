9fe9699e77fd9b79838efe79d633f5038a41c505
Suraj Vijay <surajcpp115@gmail.com>
2022-04-04 23:43:06 +0530

    default image for users

--- src/components/edit-user-form/index.tsx
+++ src/components/edit-user-form/index.tsx
@@ -1,17 +1,10 @@
 import React, {useState} from 'react';
-import styled from 'styled-components';
 
 import Section from "../section";
 import Input from '../input';
 import Button from '../button';
 
-const Wrapper = styled.div`
-  display: flex;
-  align-self: center;
-  flex-direction: column;
-  width: 50%;
-  gap: 0.5rem;
-`;
+import {Wrapper, ImageArea, ProfileImage} from './styles';
 
 interface EditUserFormProps {}
 
@@ -19,6 +12,11 @@ const EditUserForms: React.FC<EditUserFormProps> = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const getImageUrl = () => {
+    setImageUrl(window.prompt("URL of image: ") as string);
+  };
 
   return (
     <div>
@@ -33,7 +31,15 @@ const EditUserForms: React.FC<EditUserFormProps> = () => {
           setValue={setPassword}
           isPass
         />
-        <Button variant={4} text="Change profile picture" disabled={false} onPress={() => {}} />
+        <ImageArea>
+          <Button
+            variant={4}
+            text="Change profile picture"
+            disabled={false}
+            onPress={getImageUrl}
+          />
+          <ProfileImage src={imageUrl} />
+        </ImageArea>
         <Button variant={2} text="UPDATE" disabled={false} onPress={() => {}} />
       </Wrapper>
     </div>

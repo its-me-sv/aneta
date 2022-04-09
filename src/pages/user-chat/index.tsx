19398acc7f34e7206de7da99bfa337703bf55bef
Suraj Vijay <surajcpp115@gmail.com>
2022-04-09 15:53:54 +0530

    joined check at user chat

--- src/pages/user-chat/index.tsx
+++ src/pages/user-chat/index.tsx
@@ -1,45 +1,43 @@
 import React, {useEffect, useState} from "react";
-import styled from "styled-components";
+import axios from "axios";
+
+import {MainContainer, RightContainer, NotSelected} from './styles';
 
 import NavUser from "../../components/nav-user";
 import ChatScreen from "../../components/chat-screen";
 import Contacts from "../../components/contacts";
 
+import {useUserContext} from "../../contexts/user.context";
+import {useAPIContext} from "../../contexts/api.context";
 import {useUserNavContext} from "../../contexts/user-nav.context";
-
-const MainContainer = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 10fr;
-`;
-
-const RightContainer = styled.div`
-  display: grid;
-  grid-template-columns: 3fr 1fr;
-`;
-
-const NotSelected = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-family: calibri;
-  font-size: 4.2rem;
-  opacity: 0.5;
-  text-align: center;
-`;
 
 interface UserChatPageProps {}
 
 const UserChatPage: React.FC<UserChatPageProps> = () => {
   const {changeUni} = useUserNavContext();
+  const {REST_API} = useAPIContext();
+  const {id, token, setLoading} = useUserContext();
   const [currChat, setCurrChat] = useState<string>('');
+  const [joined, setJoined] = useState<boolean>(false);
   
   useEffect(() => {
     changeUni!(1);
   }, []);
 
+  useEffect(() => {
+    if (!id.length || !token.length) return;
+    setLoading!(true);
+    axios.post(`${REST_API}/employee/joined`, {id}, {
+      headers: {Authorization: `Bearer ${token}`}
+    }).then(({data}) => {
+      setJoined(data.joined);
+      setLoading!(false);
+    }).catch(() => setLoading!(false));
+  }, [id, token]);
+
   return (
     <MainContainer>
-      <NavUser />
+      <NavUser joined={joined} />
       <RightContainer>
         {currChat.length 
         ? <ChatScreen chatId={currChat} /> 

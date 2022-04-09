a73dca3187a7420a815861d65be4746e0b9a072d
Suraj Vijay <surajcpp115@gmail.com>
2022-04-09 07:09:03 +0530

    employee actions inital state fetch

--- src/components/user-actions/index.tsx
+++ src/components/user-actions/index.tsx
@@ -1,44 +1,66 @@
-import React from "react";
-import styled from "styled-components";
+import React, {useState, useEffect, useRef} from "react";
+import axios from 'axios';
+
+import {ActionButtons} from './styles';
 
 import Section from "../section";
 import Button from "../button";
 
-const ActionButtons = styled.div`
-  margin-top: 1%;
-  display: flex;
-  gap: 1rem;
-`;
+import {useAPIContext} from '../../contexts/api.context';
+import {useUserContext} from '../../contexts/user.context';
 
 interface UserActionsProps {}
 
 const UserActions: React.FC<UserActionsProps> = () => {
-    const onClick = () => window.alert("button clicked");
-    return (
-      <div>
-        <Section name="Actions" />
-        <ActionButtons>
-          <Button
-            onPress={onClick}
-            disabled={false}
-            variant={1}
-            text="SET AWAY"
-          />
-          <Button
-            onPress={onClick}
-            disabled={false}
-            variant={2}
-            text="SET ONLINE"
-          />
-          <Button
-            onPress={onClick}
-            disabled={false}
-            variant={3}
-            text="REQUEST LEAVE"
-          />
-        </ActionButtons>
-      </div>
-    );
+  const {REST_API} = useAPIContext();
+  const {token, id, setLoading} = useUserContext();
+  const [status, setStatus] = useState<number>(2);
+  const [request, setRequest] = useState<boolean>(false);
+  const fetched = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (!token.length || !id.length) return;
+    if (fetched.current) return;
+    fetched.current = true;
+    setLoading!(true);
+    axios.post(`${REST_API}/employee/stat-req`, {id}, {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then(({data}) => {
+      setStatus(data.status);
+      setRequest(data.request);
+      setLoading!(false);
+    })
+    .catch(() => setLoading!(false));
+  }, [token, id]);
+  
+  const onClick = () => window.alert("button clicked");
+  
+  return (
+    <div>
+      <Section name="Actions" />
+      <ActionButtons>
+        <Button
+          onPress={onClick}
+          disabled={status===1}
+          variant={1}
+          text="SET AWAY"
+        />
+        <Button
+          onPress={onClick}
+          disabled={status===2}
+          variant={2}
+          text="SET ONLINE"
+        />
+        <Button
+          onPress={onClick}
+          disabled={request}
+          variant={3}
+          text="REQUEST LEAVE"
+        />
+      </ActionButtons>
+    </div>
+  );
 };
 
 export default UserActions;

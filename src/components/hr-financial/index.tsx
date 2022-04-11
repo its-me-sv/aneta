e5bcad4990ffeff31c6e7208e2a8e8d1f0a889aa
Suraj Vijay <surajcpp115@gmail.com>
2022-04-11 17:05:45 +0530

    financial stats pulling from db

--- src/components/hr-financial/index.tsx
+++ src/components/hr-financial/index.tsx
@@ -1,36 +1,72 @@
-import React from "react";
+import React, {useEffect, useState, useRef} from 'react';
+import axios from 'axios';
 
 import {Section, SectionContent, Content} from "../hr-sections/styles";
 
 import HRSections from "../hr-sections";
 import {VrtclLn} from "../switch-form";
 
+import {useAPIContext} from '../../contexts/api.context';
+import {useOrganisationContext} from '../../contexts/organisation.context';
+
+interface FinancialStats {
+  today: string;
+  total: string;
+  transactions: string;
+}
+
+const defaultState: FinancialStats = {
+  today: "-",
+  total: "-",
+  transactions: "-",
+};
+
 interface HRFinancialProps {
   onPress: () => void;
 }
 
 const HRFinancial: React.FC<HRFinancialProps> = ({onPress}) => {
+  const {REST_API} = useAPIContext();
+  const {token, orgName} = useOrganisationContext();
+  const [state, setState] = useState<FinancialStats>(defaultState);
+  const fetched = useRef<boolean>(false);
+
+  const fetchData = () => {
+    axios.post(`${REST_API}/transactions/stats`, {orgName}, {
+      headers: {Authorization: `Bearer ${token}`,}
+    }).then(({data}) => {
+      setState(data);
+    }).catch(() => {});
+  };
+
+  useEffect(() => {
+    if (!token.length || !orgName.length) return;
+    if (fetched.current) return;
+    fetched.current = true;
+    fetchData();
+  }, [orgName, token]);
+  
   return (
     <Section onClick={onPress}>
       <HRSections variant={5} />
       <SectionContent>
         <Content>
           <span>Today</span>
-          <span>4 $</span>
+          <span>{state.today} $</span>
         </Content>
         <div>
           <VrtclLn />
         </div>
         <Content>
           <span>Total</span>
-          <span>8 $</span>
+          <span>{state.total} $</span>
         </Content>
         <div>
           <VrtclLn />
         </div>
         <Content>
           <span>No. of Transactions</span>
-          <span>8</span>
+          <span>{state.transactions}</span>
         </Content>
       </SectionContent>
     </Section>

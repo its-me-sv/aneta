7dd2ca0048fdeb4ba0ae201fd07eff04d818adf8
Suraj Vijay <surajcpp115@gmail.com>
2022-04-11 16:58:42 +0530

    resources stats pulling from db

--- src/components/hr-resources/index.tsx
+++ src/components/hr-resources/index.tsx
@@ -1,50 +1,90 @@
-import React from "react";
+import React, {useEffect, useState, useRef} from 'react';
+import axios from "axios";
 
-import { Section, SectionContent, Content } from "../hr-sections/styles";
+import {Section, SectionContent, Content} from "../hr-sections/styles";
 
 import HRSections from "../hr-sections";
-import { VrtclLn } from "../switch-form";
+import {VrtclLn} from "../switch-form";
+
+import {useAPIContext} from '../../contexts/api.context';
+import {useOrganisationContext} from '../../contexts/organisation.context';
+
+interface ResourceStats {
+  Developer: string;
+  Tester: string;
+  Support: string;
+  "Project Manager": string;
+  Candidates: string;
+}
+
+const defaultState: ResourceStats = {
+  Developer: "-",
+  Tester: "-",
+  Support: "-",
+  "Project Manager": "-",
+  Candidates: "-",
+};
 
 interface HRResourcesProps {
   onPress: () => void;
 }
 
 const HRResources: React.FC<HRResourcesProps> = ({onPress}) => {
+  const {REST_API} = useAPIContext();
+  const {token, orgName} = useOrganisationContext();
+  const [state, setState] = useState<ResourceStats>(defaultState);
+  const fetched = useRef<boolean>(false);
+
+  const fetchData = () => {
+    axios.post(`${REST_API}/employee/stats`, {orgName}, {
+      headers: {Authorization: `Bearer ${token}`,}
+    }).then(({data}) => {
+      setState(data);
+    }).catch(() => {});
+  };
+
+  useEffect(() => {
+    if (!token.length || !orgName.length) return;
+    if (fetched.current) return;
+    fetched.current = true;
+    fetchData();
+  }, [orgName, token]);
+
   return (
     <Section onClick={onPress}>
       <HRSections variant={4} />
       <SectionContent>
         <Content>
-          <span>Developer</span>
-          <span>1</span>
+          <span>Developers</span>
+          <span>{state.Developer}</span>
         </Content>
         <div>
           <VrtclLn />
         </div>
         <Content>
-          <span>Tester</span>
-          <span>1</span>
+          <span>Testers</span>
+          <span>{state.Tester}</span>
         </Content>
         <div>
           <VrtclLn />
         </div>
         <Content>
           <span>Support</span>
-          <span>1</span>
+          <span>{state.Support}</span>
         </Content>
         <div>
           <VrtclLn />
         </div>
         <Content>
-          <span>Project Manager</span>
-          <span>1</span>
+          <span>Project Managers</span>
+          <span>{state['Project Manager']}</span>
         </Content>
         <div>
           <VrtclLn />
         </div>
         <Content>
           <span>Candidates</span>
-          <span>4</span>
+          <span>{state.Candidates}</span>
         </Content>
       </SectionContent>
     </Section>

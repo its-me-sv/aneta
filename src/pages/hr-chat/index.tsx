4e098fc105e9a3c8aef36de1c07ba047cdbd14cf
Suraj Vijay <surajcpp115@gmail.com>
2022-04-11 14:41:04 +0530

    separated styles and code from hr-chat

--- src/pages/hr-chat/index.tsx
+++ src/pages/hr-chat/index.tsx
@@ -1,31 +1,12 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+
+import {MainContainer, RightContainer, NotSelected} from './styles';
 
 import NavHR from "../../components/nav-hr";
 import ChatScreen from "../../components/chat-screen";
 import Contacts from "../../components/contacts";
 
 import {useUserNavContext} from "../../contexts/user-nav.context";
-
-const MainContainer = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 10fr;
-`;
-
-const RightContainer = styled.div`
-  display: grid;
-  grid-template-columns: 3fr 1fr;
-`;
-
-const NotSelected = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-family: calibri;
-  font-size: 4.2rem;
-  opacity: 0.5;
-  text-align: center;
-`;
 
 interface HRChatPageProps {}
 

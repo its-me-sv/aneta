9c8e681efde62c50c87c81abfee69693cb03e588
Suraj Vijay <surajcpp115@gmail.com>
2022-04-12 09:16:02 +0530

    styles separated on logout

--- src/components/logout/index.tsx
+++ src/components/logout/index.tsx
@@ -1,34 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import logoutIcon from "../../assets/icons/logout.png";
+import {LogoutSection, LogoutIcon, LogoutText} from './styles';
+
 import {useOrganisationContext} from '../../contexts/organisation.context';
 import {useUserContext} from '../../contexts/user.context';
-
-const LogoutSection = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 0.42rem;
-  right: 1%;
-  top: 2%;
-  cursor: pointer;
-`;
-
-const LogoutIcon = styled.img.attrs({
-  alt: '',
-  src: logoutIcon
-})`
-  width: 1.6rem;
-  height: 1.6rem;
-`;
-
-const LogoutText = styled.span`
-  font-family: bahnschrift;
-  font-size: 1.2rem;
-  opacity: 0.7;
-`;
 
 interface LogoutProps {}
 
